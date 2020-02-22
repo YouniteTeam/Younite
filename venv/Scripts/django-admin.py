147de026589854035a46bf7b1025807c2cc3b880
Kynemo <35339148+Kynemo@users.noreply.github.com>
2020-02-22 18:20:53 +0000

    Adjustment

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Oscar\OneDrive\Documents\Github\Younite\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
